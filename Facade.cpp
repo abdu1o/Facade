7d05312f8629bc1fd28a0d45ac18ab7c0f9e6e98
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-06-09 14:37:05 +0300

    Добавьте файлы проекта.

---
+++ Facade.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+#include "SubSystems.h"
+
+class CinemaFacade 
+{
+private:
+    
+    Projector _projector;
+    SoundSysytem _sound;
+    DvdPlayer _dvd;
+    Screen _screen;
+    string _title;
+
+
+public:
+
+    void SetMovie(const string title)
+    {
+        this->_title = title;
+    }
+
+    void WatchMovie() 
+    {
+        cout << "U watching movie \"" << _title << "\" " << endl;
+        _projector.On();
+        _sound.On();
+        _dvd.On();
+        _screen.On();
+    }
+
+    void EndMovie()
+    {
+        cout << "Thanks for watching!" << endl;
+        _projector.Off();
+        _sound.Off();
+        _dvd.Off();
+        _screen.Off();
+    }
+};
+
+int main()
+{
+    CinemaFacade cinema;
+    cinema.SetMovie("Doctor Who");
+    cinema.WatchMovie();
+    cout << endl;
+    cinema.EndMovie();
+}
---
+++ SubSystems.h
@@ -0,0 +1,61 @@
+#pragma once
+
+class Projector
+{
+public:
+
+	void On()
+	{
+		cout << "Projector turn on!" << endl;
+	}
+
+	void Off()
+	{
+		cout << "Projector turn off!" << endl;
+	}
+};
+
+class SoundSysytem
+{
+public:
+
+	void On()
+	{
+		cout << "Sound turn on!" << endl;
+	}
+
+	void Off()
+	{
+		cout << "Sound turn off!" << endl;
+	}
+};
+
+class DvdPlayer
+{
+public:
+
+	void On()
+	{
+		cout << "Player turn on!" << endl;
+	}
+
+	void Off()
+	{
+		cout << "Player turn off!" << endl;
+	}
+};
+
+class Screen
+{
+public:
+
+	void On()
+	{
+		cout << "Screen turn on!" << endl;
+	}
+
+	void Off()
+	{
+		cout << "Screen turn off!" << endl;
+	}
+};
